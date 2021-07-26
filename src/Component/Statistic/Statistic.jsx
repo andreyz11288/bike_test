a8042982eff8c879faea191ef8e339aa93535c4f
andreyz11288 <andreyz11288@gmail.com>
2021-07-26 12:39:32 +0300

    fix

--- src/Component/Statistic/Statistic.jsx
+++ src/Component/Statistic/Statistic.jsx
@@ -10,25 +10,25 @@ export default function Statistic() {
   const [stateBooked, setBooked] = useState(null);
 
 
-  useEffect(() => {
-    if (state & state.length > 0) {
-      const available = state.map(e => e.status === 'available');
-      const ava = available.reduce((total, amount) => total + amount);
-      ava === true ? setAvailable(1) : setAvailable(ava);
-      !ava && setAvailable(0);
-      const booked = state.map(e => e.status === 'busy');
-      const bookedBikes = booked.reduce((total, amount) => total + amount);
-      bookedBikes === true ? setBooked(1) : setBooked(bookedBikes);
-      !bookedBikes && setBooked(0);
-      const average = state.map(e => {
-        const price = Number(e.price);
-        return price;
-      });
-      const ave =
-        average.reduce((total, amount) => total + amount) / state.length;
-      setAve(Math.round(ave * 100.0) / 100.0);
-    }
-  }, [state]);
+  // useEffect(() => {
+  //   if (state & state.length > 0) {
+  //     const available = state.map(e => e.status === 'available');
+  //     const ava = available.reduce((total, amount) => total + amount);
+  //     ava === true ? setAvailable(1) : setAvailable(ava);
+  //     !ava && setAvailable(0);
+  //     const booked = state.map(e => e.status === 'busy');
+  //     const bookedBikes = booked.reduce((total, amount) => total + amount);
+  //     bookedBikes === true ? setBooked(1) : setBooked(bookedBikes);
+  //     !bookedBikes && setBooked(0);
+  //     const average = state.map(e => {
+  //       const price = Number(e.price);
+  //       return price;
+  //     });
+  //     const ave =
+  //       average.reduce((total, amount) => total + amount) / state.length;
+  //     setAve(Math.round(ave * 100.0) / 100.0);
+  //   }
+  // }, [state]);
 
   console.log(state);
   return (
